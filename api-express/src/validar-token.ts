b9b513c511e1e96e95f6bc2f474a0a6d5b24aca1
Leandro Berto <leandroberto2010@gmail.com>
2023-11-02 16:40:35 -0300

    Login simple y generacion de token

---
+++ api-express/src/validar-token.ts
@@ -0,0 +1,18 @@
+import {Request, Response, NextFunction} from 'express'
+import jwt from 'jsonwebtoken'
+
+const validateToken = (req: Request, res:Response, next:NextFunction) => {
+    const headerToken= req.headers['authorization'];
+    if(headerToken != undefined){ //puedo ponerle que tambien empiece con Bearer
+        try{
+            const BearerToken=headerToken.slice(7);
+            jwt.verify(BearerToken, process.env.SECRET_KEY || 'troleado');
+            next();
+        } catch(error) {
+            res.status(401).json({msg: 'token no valido'})
+        }
+    } else{
+        res.status(401).json({msg: 'Acceso denegado'});
+    }
+
+}
