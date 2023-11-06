f75d815459b368520e44292ee0293a8c426323cd
pecorarolucio <luciopecoraro2002@gmail>
2023-11-06 17:01:18 -0300

    CRUD plato

---
+++ api-express/src/plato/plato.data.ts
@@ -0,0 +1,75 @@
+import { parseArgs } from 'util';
+import { Plato } from './plato.model.js';
+//import { Op } from 'sequelize';
+
+
+export class PlatoRepository {
+  public async findAll(): Promise<Plato[] | undefined> {
+    try {
+      const platos = await Plato.findAll();
+      return platos;
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  public async findOne(item: { id: string }): Promise<Plato | undefined> {
+    try {
+      const plato = await Plato.findByPk(parseInt(item.id));
+      if (!plato) {
+        return undefined;
+      }
+      return plato;
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  public async add(item: any): Promise<Plato | undefined> {
+    try {
+      const nuevoPlato = await Plato.create(item);
+      return nuevoPlato;
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  public async update(id: string, item: any): Promise<Plato | undefined> {
+    try {
+      await Plato.update(item, {
+        where: {
+          id: Number.parseInt(id),
+        },
+      });
+      const platoActualizado = await this.findOne({ id });
+      return platoActualizado;
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  public async delete(item: { id: string }): Promise<number> {
+    try {
+      const eliminadas = await Plato.destroy({
+        where: {
+          id: parseInt(item.id),
+        },
+      });
+      return eliminadas;
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  /*public async findPlatoDelDia(item: { fecha: String }): Promise<Plato | undefined> {
+    try {
+      const plato = await Plato.findOne({ where: { fecha:  } });
+      if (!plato || plato.dataValues.tipo != 'plato') {
+        return undefined;
+      }
+      return plato;
+    } catch (error) {
+      throw error;
+    }
+  }*/
+  }
---
+++ api-express/src/plato/plato.routes.ts
@@ -0,0 +1,17 @@
+import { Router } from 'express';
+import {
+  add,
+  findAll,
+  findOne,
+  remove,
+  sanitizePlatoInput,
+  update,
+} from './plato.controller.js';
+
+export const platoRouter = Router();
+
+platoRouter.get('/', findAll);
+platoRouter.get('/:id', findOne);
+platoRouter.post('/', sanitizePlatoInput, add);
+platoRouter.put('/:id', sanitizePlatoInput, update);
+platoRouter.delete('/:id', remove);
