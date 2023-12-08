2fd9e9d5158e298a5d61e03b6cb99414154d8414
Leandro Berto <leandroberto2010@gmail.com>
2023-12-08 14:43:23 -0300

    Added estilos a todos los menu
    Le agregue estilos a todos los menu, falta un poco en algunos pero lo general esta hecho

--- api-express/dist/cliente/cliente.routes.js
+++ api-express/dist/cliente/cliente.routes.js
@@ -1,17 +1,11 @@
 import { Router } from 'express';
 import { validateToken } from '../validar-token.js';
-import { add, findAll, findOne, findByDni, login, remove, sanitizeClienteInput, update, } from './cliente.controller.js';
+import { add, findAll, findByDni, findOne, login, remove, sanitizeClienteInput, update, } from './cliente.controller.js';
 export const clienteRouter = Router();
 clienteRouter.get('/', validateToken, findAll);
-<<<<<<< HEAD
 clienteRouter.get('/:id', validateToken, findOne);
 clienteRouter.post('/', sanitizeClienteInput, add);
 clienteRouter.put('/:id', validateToken, sanitizeClienteInput, update);
-=======
-clienteRouter.get('/:id', findOne);
-clienteRouter.post('/', validateToken, sanitizeClienteInput, add);
-clienteRouter.put('/:id', sanitizeClienteInput, update);
->>>>>>> e094c3b56c1e7383919956fa25943856ef2064a3
 clienteRouter.delete('/:id', validateToken, remove);
 clienteRouter.post('/login', login);
 clienteRouter.get('/dni/:dni', findByDni);
