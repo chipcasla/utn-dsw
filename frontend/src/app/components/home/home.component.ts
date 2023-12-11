1ebe4c13900483da217f9446f4d635eb47bb444c
Leandro Berto <leandroberto2010@gmail.com>
2023-12-11 12:29:11 -0300

    added home agregue home, cambie un par de cosas de algunos botones, y elimine add-reserva que no se usaba

--- frontend/src/app/components/home/home.component.ts
+++ frontend/src/app/components/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ClienteService } from 'app/services/cliente.service';
 
 @Component({
   selector: 'app-home',
@@ -6,5 +7,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.css'],
 })
 export class HomeComponent {
-  constructor() {}
+  cliente: any;
+
+  constructor(private clienteService: ClienteService) {}
+
+  ngOnInit(): void{
+    this.loadCliente();
+  }
+
+  loadCliente(){
+    const idCliente = this.clienteService.getUserId(); 
+    this.clienteService.findOne(idCliente).subscribe(cliente =>{
+      this.cliente = cliente
+    })
+  }
 }
